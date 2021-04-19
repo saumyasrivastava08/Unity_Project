f8f82ab09f63adee2d8f7aa415404ca0bda8f21e
Saumyanshu <saumyasanjay08@gmail.com>
2021-04-19 14:59:06 +0530

    game_ca

---
+++ Assets/Scripts/MainMenuScript.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuScript : MonoBehaviour
+{
+    public void SceneLoader()
+    {
+        SceneManager.LoadScene(1);
+    }
+    public void ApplictionQuitter()
+    {
+        Application.Quit();
+    }
+}
